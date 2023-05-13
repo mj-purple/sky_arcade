708cba7cf213ce5789f85db8b3a154fc2da9b832
mj <mj@pop-os.localdomain>
2023-05-13 02:56:56 +0200

    carpeta games

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JuegoPiedraPapelTijerasComponent } from './juego-piedra-papel-tijeras/juego-piedra-papel-tijeras.component';
+import { RPSComponent } from './games/rps/rps.component';
+import { JuegoPiedraPapelTijeraComponent } from './games/juego-piedra-papel-tijera/juego-piedra-papel-tijera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JuegoPiedraPapelTijerasComponent,
+    RPSComponent,
+    JuegoPiedraPapelTijeraComponent
   ],
   imports: [
     BrowserModule,
