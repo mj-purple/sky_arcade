8a93d0ee316b514302c9976d1deda26380bd2725
IagoPL <iagoprietolamas@gmail.com>
2023-05-13 08:36:39 +0200

    ya esta  arreglado

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { JuegoPiedraPapelTijerasComponent } from './juego-piedra-papel-tijeras/juego-piedra-papel-tijeras.component';
-import { RPSComponent } from './games/rps/rps.component';
 import { JuegoPiedraPapelTijeraComponent } from './games/juego-piedra-papel-tijera/juego-piedra-papel-tijera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JuegoPiedraPapelTijerasComponent,
-    RPSComponent,
-    JuegoPiedraPapelTijeraComponent
+
   ],
   imports: [
     BrowserModule,
